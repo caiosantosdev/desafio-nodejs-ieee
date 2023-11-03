834ee76845cb3c5d7c432c411e845e1097b44231
Caio Santos <caiosantosdev@gmail.com>
2023-11-03 17:52:30 -0300

    add migrations finally

--- knexFile.js
+++ knexFile.js
@@ -1,23 +1,19 @@
 const knexConfiguration = {
   client: 'mysql',
   development:{
-      client: 'mysql',
-      connection: {
-        database : 'mercado',
-        version: '8.0.28',
-        host : 'localhost',
-        port : 3306,
-        user : 'root',
-        password : 'ieeevents'
-      },
-      migrations: {
-        tableName: 'migrations',
-        directory: './src/database/migrations'
-      }
+    client: 'mysql',
+    connection: {
+      database : 'mercado',
+      version: '8.0.28',
+      host : 'localhost',
+      port : 3306,
+      user : 'root',
+      password : 'ieeevents'
+    },
+    migrations: {
+      directory: ('./src/database/migrations/')
+    }
   }
 }
-
-
-
 
 module.exports = knexConfiguration;
